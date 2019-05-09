ce39093e9791f6ad197cf6be80b55de8298573ee
chenyan <522581280@qq.com>
2019-05-09 11:14:43 +0800

    添加Feign Hystrix熔断

--- eureka-client-consumer/src/main/java/com/cherry/demo/eurekaclientconsumer/HelloRemote.java
+++ eureka-client-consumer/src/main/java/com/cherry/demo/eurekaclientconsumer/HelloRemote.java
@@ -10,7 +10,13 @@ import org.springframework.web.bind.annotation.RequestParam;
  * User: chenyan
  * Date: 2019/5/6 6:31 PM
  */
-@FeignClient(name="eureka-client-product")//声明web接口调用的fegin客户端
+
+/*
+ * @FeignClient:声明web接口调用的fegin客户端
+ * name:是eureka服务提供者注册到Eureka服务里的名称
+ * fallback:指明熔断后，执行的操作
+ */
+@FeignClient(name="eureka-client-product",fallback = HelloRemoteHystrix.class)
 public interface HelloRemote {
 
     @RequestMapping("/hello")
--- eureka-client-consumer/src/main/java/com/cherry/demo/eurekaclientconsumer/HelloRemoteHystrix.java
+++ eureka-client-consumer/src/main/java/com/cherry/demo/eurekaclientconsumer/HelloRemoteHystrix.java
@@ -1,13 +1,17 @@
 package com.cherry.demo.eurekaclientconsumer;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
- * Describe:
+ * Describe:HelloRemote熔断操作
  * User: chenyan
  * Date: 2019/5/9 10:50 AM
  */
+@Component
 public class HelloRemoteHystrix implements HelloRemote {
     @Override
-    public String hello(String name) {
-        return null;
+    public String hello(@RequestParam("name") String name) {
+        return "hello " +name+", this messge send failed .";
     }
 }
